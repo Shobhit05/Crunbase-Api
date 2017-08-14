575189df37bfb6592159d3cd5318121f133e1a2f
Shobhit Srivastava <shobhit.srivastava39@gmail.com>
2017-08-14 11:48:46 +0530

    Update extract_info.py

--- extract_info.py
+++ extract_info.py
@@ -1,7 +1,7 @@
 from crunchbaseapi import *
 import pprint
 import time
-a=CrunchBase('d900b3954e457c3d7491c2ed79ba2b93')
+a=CrunchBase('*PASTE YOUR KEY HERE*')
 
 a.get_company_people('startupflux')
 
