35f83ce666f3ca4c05f4ee99aaff46189837aa89
Shobhit Srivastava <shobhit.srivastava39@gmail.com>
2017-08-16 16:23:27 +0530

    Update crunchbaseapi.py

--- crunchbaseapi.py
+++ crunchbaseapi.py
@@ -21,7 +21,7 @@ class CrunchBase():
 
     def updated_companies(self):
         epoch_date=str(int(datetime.datetime.now().timestamp()))
-        req=requests.get("https://api.crunchbase.com/v3/odm-organizations?user_key=d900b3954e457c3d7491c2ed79ba2b93&updated_since="+epoch_date)
+        req=requests.get("https://api.crunchbase.com/v3/odm-organizations?user_key="+key+"&updated_since="+epoch_date)
         json_data = json.loads(req.text)
         return(json_data)
 
